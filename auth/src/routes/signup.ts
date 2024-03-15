338c4baf62c27f9b9275689419264512003eae8f
germano <germano@debian12.debian12>
2024-03-14 22:01:20 -0300

    create validation to email and password

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,17 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
+router.post('/api/users/signup', [
+    body('Email')
+        .isEmail()
+        .withMessage('Please, provide a valid email.'),
+    body('Password')
+        .trim()
+        .isLength({ min: 4, max: 20})
+        .withMessage('Password must contain between 4 to 20 characters')
+], (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
