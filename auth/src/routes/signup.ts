82f27eab0401851b05ad4fcaef51e23bf8a65f47
germano <germano@debian12.debian12>
2024-03-15 22:14:15 -0300

    add more valitdation do user sign up

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { body } from 'express-validator';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
@@ -12,7 +12,16 @@ router.post('/api/users/signup', [
         .isLength({ min: 4, max: 20})
         .withMessage('Password must contain between 4 to 20 characters')
 ], (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).send(errors.array());
+    };
+
+    console.log('Creating a user...');
     res.send('Hello World');
+
+    res.send({});
 });
 
 export { router as signupRouter };
