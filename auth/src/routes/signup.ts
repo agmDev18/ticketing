273086983647cb573230e97ff1a70baca909c539
germano <germano@debian12.debian12>
2024-03-20 22:36:53 -0300

    add error-handler

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -15,11 +15,11 @@ router.post('/api/users/signup', [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(400).send(errors.array());
+        throw new Error('Invalid email or password');
     };
 
     console.log('Creating a user...');
-
+   // throw new Error(`Can't connect to the database`);
     res.send({});
 });
 
