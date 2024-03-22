f03d1282371c7dfa515bae858cb38497440f1974
germano <germano@debian12.debian12>
2024-03-21 22:10:46 -0300

    determine error type

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,10 @@
+import { ValidationError } from 'express-validator';
+
+export class RequestValidationError extends Error {
+    constructor(public error: ValidationError[]) {
+        super();
+
+        // only because we are extending a buit in class
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+}
--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -1,9 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('something went wrong', err);
+export const errorHandler = (
+    err: Error, 
+    req: Request, 
+    res: Response, 
+    next: NextFunction ) => {
+    
+        if (err instanceof RequestValidationError) {
+            console.log('handling this error as request valitdation error');
+        }
 
-    res.status(400).send({
-        message: err.message
-    });
+        if (err instanceof DatabaseConnectionError) {
+            console.log('handling this error as database connection error');
+        }
+
+
+        res.status(400).send({
+            message: err.message
+        });
 };
